0a25c80a35c4450b78b2950a6249897f91633257
Ebenezer Ogungbe <ogungbesegun2000@gmail.com>
2020-08-12 14:10:43 +0100

    delete btn initial fix

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,15 +1,15 @@
 // https://eb-kneezer.github.io/World-Clock-Web-App/
 
 // Declaration of Variables
+
 let today, 
 newLocation, 
 newTime, 
 displayTime, 
-newCity;
+counter;
 
-let counter = -1;
 let availableCity = [];
-newCity = [];
+let newCity = [];
 
 time_zones.forEach(zone => {
   availableCity.push(zone.city);
@@ -22,24 +22,31 @@ document.querySelector('#send').addEventListener('click', () => {
     time_zones.forEach(zone => {
         if (similar(zone.city, newLocation) >= 60) {
             newCity.push(zone);
-            counter += 1;
-            // getTime();
+            counter = 'no' + newCity.indexOf(zone);
             displayContent();
         }
     });
 });
 
 
+
+
 // add new myTimeZone section
 function displayContent() {
     document.querySelector('.my-time-zone .container').innerHTML += `<div class="city-section" id="${counter}"><h4 class="text-uppercase font-weight-bold"></h4><div class="date-time"><div class="time"></div><div class="location"></div></div><i class="delete-time fa fa-times-circle" id="${counter}"></i></div>`;
-    document.querySelectorAll('.delete-time')
-    .forEach(button => {
-        button.addEventListener('click', ()=>{
-            
-        })
-    })
+    
     getTime();
+
+    document.querySelectorAll('.delete-time').forEach(button => {
+        button.addEventListener('click', ()=>{
+            let attr = Number(button.getAttribute('id').split('')[2]);
+            newCity.splice(attr, 1);
+            deleteCity(attr);
+            console.log(newCity);
+            getTime();
+        });
+    });
+
 }
 
 // Generate current time.
@@ -61,13 +68,14 @@ function getTime() {
 }
 
 // remove timeZone section
-//not yet working
-// let el = document.querySelector('.fa');
-// if (el) {
-//   el.addEventListener('click', () => {
-//   alert('clicked')
-// });
-// }
+//(strangen bugs still exist)
+function deleteCity(btnId) {
+    if(document.querySelectorAll('.city-section')[btnId]){
+        document.querySelectorAll('.city-section')[btnId].remove();
+    } else {
+        document.querySelector('.city-section').remove();
+    }
+}
        
 //for single digit time, add '0' in front
 function checkTime(i) {
@@ -84,8 +92,8 @@ function similar(a,b) {
     var lengthA = a.length;
     var lengthB = b.length;
     var equivalency = 0;
-    var minLength = (a.length > b.length) ? b.length : a.length;    
-    var maxLength = (a.length < b.length) ? b.length : a.length;    
+    var minLength = (lengthA > lengthB) ? lengthB : lengthA;    
+    var maxLength = (lengthA < lengthB) ? lengthB : lengthA;    
     for(var i = 0; i < minLength; i++) {
         if(a[i] == b[i]) {
             equivalency++;
